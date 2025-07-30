f6974bd70a507309d8fda2d4c691f839fbe55b6c
Oleksii Slieptsov <45471723-artur220513timu@users.noreply.replit.com>
2025-07-30 15:04:17 +0000

    Post-Recovery checkpoint

--- replit.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs }: {
-  deps = [
-    pkgs.python311
-    pkgs.python311Packages.pip
-    pkgs.python311Packages.gspread
-    pkgs.python311Packages.oauth2client
-    pkgs.python311Packages.python-dotenv
-  ];
-}
